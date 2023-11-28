c4e5138e49c33a554b714adec57987add8cbaf94
Donald Ryan Gullett <45299186+MaxxRK@users.noreply.github.com>
2023-11-28 06:50:08 -0600

    initial response string parsed

--- chase/account.py
+++ chase/account.py
@@ -1,6 +1,7 @@
 
 import gzip
 import json
+from enum import Enum
 
 from selenium.common import exceptions
 from selenium.webdriver.common.by import By
@@ -11,38 +12,69 @@ from chase.session import ChaseSession
 from chase.urls import account_info, get_headers, landing_page
 
 
-class Account:
+class AllAccount:
     def __init__(self, session: ChaseSession):
         self.session = session
         self.requestid = None
+        self.all_account_info = self.get_all_account_info()
+        self.account_connectors = self.get_account_connectors()
+        
 
-    def print_request_id(self, event):
-        self.requestid = event["requestId"]
-        print("Request ID: ", event["requestId"])
-    
-     
-    def get_account_info(self):
-        #log = 'performance'
-        #self.session.driver.execute_cdp_cmd("Network.enable", {})
+    def get_all_account_info(self):
         self.session.driver.get(landing_page())
         for request in self.session.driver.requests:
             if request.response:
                 if request.url == account_info():
                     body = request.response.body
                     body = gzip.decompress(body).decode('utf-8')
-                    print(
-                        request.url,
-                        request.response.status_code,
-                        body
-                    )
-        #try:
-            #log = json.loads(log["message"])["message"]
-            #if ("Network.responseReceived" in log["method"] and "params" in log.keys()):
-                #body = self.session.driver.execute_cdp_cmd('Network.getResponseBody', {'requestId': log["params"]["requestId"]})
-        #except exceptions.WebDriverException:
-            #print('response.body is null')
-        #request_id = self.session.driver.execute_script("return window.requestId;")
-        #print(request_id)
-        #response = self.session.driver.execute_cdp_cmd("Network.getResponseBody", {"requestId": request_id})
-        #print(response)
+                    account_json = json.loads(body)
+                    for info in account_json['cache']:
+                        if info['url']== '/svc/rr/accounts/secure/v1/account/detail/inv/list':
+                            invest_json = info['response']['chaseInvestments']
+                    if request.response.status_code == 200:
+                        self.total_value = invest_json['investmentSummary']['accountValue']
+                        self.total_value_change = invest_json['investmentSummary']['accountValueChange']
+                        return invest_json
+                    else:
+                        return None
+    
+    def get_account_connectors(self):
+        account_dict = {}
+        for item in self.all_account_info['accounts']:
+            account_dict[item['accountId']] = [item['mask']]
+        return account_dict
+
+           
+class AccountDetails:
+    def __init__(self, account_id, all_account: AllAccount):
+        self.all_account_info = all_account.all_account_info
+        self.account_id: str = account_id
+        self.nickname: str = None
+        self.mask: str = None
+        self.detail_type: str = None
+        self.account_value: float = None
+        self.account_value_change: float = None
+        self.eda: str = None
+        self.ira: bool = None
+        self.view_balance: bool = None
+        self.prior_year_ira: bool = None
+        self.show_xfer: bool = None
+        self.get_account_details()
+        
+    def get_account_details(self):
+        for item in self.all_account_info['accounts']:
+            if item['accountId'] == self.account_id:
+                self.mask = item['mask']
+                self.nickname = item['nickname']
+                self.detail_type = item['detailType']
+                self.account_value = float(item['accountValue'])
+                self.account_value_change = float(item['accountValueChange'])
+                self.eda = bool(item['eda'])
+                self.ira = bool(item['ira'])
+                self.view_balance = bool(item['viewBalance'])
+                self.prior_year_ira = bool(item['priorYearIra'])
+                self.show_xfer = bool(item['showXfer'])
+                   
+
+        
         
