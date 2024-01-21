fdb75ed18566ccde7a8ede87796f5003a3ce1852
Donald Ryan Gullett <45299186+MaxxRK@users.noreply.github.com>
2024-01-20 20:30:03 -0600

    update options

--- chase/session.py
+++ chase/session.py
@@ -96,8 +96,6 @@ class ChaseSession:
         """
         try:
             options = webdriver.ChromeOptions()
-            options.add_argument("--disable-infobars")
-            options.add_argument("--disable-notifications")
             if self.headless:
                 options.add_argument("--headless")
                 options.add_argument("--window-size=1920,1080")
@@ -108,13 +106,16 @@ class ChaseSession:
                 root = os.path.abspath(os.path.dirname(__file__))
                 self.profile_path = os.path.join(root, "Profile", f"Chase_{self.title}")
                 options.add_argument("user-data-dir=%s" % self.profile_path)
-            options.add_argument("--disable-gpu")
-            options.add_experimental_option("useAutomationExtension", False)
             options.add_experimental_option("excludeSwitches", ["enable-automation"])
+            options.add_experimental_option("useAutomationExtension", False)
             options.add_argument("disable-blink-features=AutomationControlled")
+            options.add_argument("--disable-notifications")
+            options.add_argument("--disable-infobars")
             options.add_argument("--log-level=3")
             if self.docker:
+                options.add_argument("--disable-dev-shm-usage")
                 options.add_argument("--no-sandbox")
+                options.add_argument("--disable-gpu")
                 chrome_service = ChromiumService("/usr/bin/chromedriver")
                 driver = webdriver.Chrome(service=chrome_service, options=options)
             else:
