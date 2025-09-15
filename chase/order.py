535117459d902a5dac02e859cf943c674b6ad1a4
MaxxRK <drgullett89@outlook.com>
2025-09-14 20:00:48 -0500

    cleanup

--- chase/order.py
+++ chase/order.py
@@ -93,11 +93,11 @@ class Order:
         """
 
         self.session.page.goto(order_page(account_id))
-        #Chase is no longer giving the option to switch from the new trading experience to the classic one.
-        #This will have to be switched to use the new experience soon.      - 9/14/2025 MAXXRK
+        # Chase is no longer giving the option to switch from the new trading experience to the classic one.
+        # This will have to be switched to use the new experience soon.      - 9/14/2025 MAXXRK
         
-        #experience = self.session.page.wait_for_selector("span > a > span.link__text")
-        #if experience.text_content() == "Switch back to classic trading experience":
+        # experience = self.session.page.wait_for_selector("span > a > span.link__text")
+        # if experience.text_content() == "Switch back to classic trading experience":
             #experience.click()
             #self.session.page.reload()
 
